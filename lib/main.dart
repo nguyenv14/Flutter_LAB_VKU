8950b0edcd7fa2d4d2fa12936831166dc098d38e
nguyen <nguyennvv.21it@vku.udn.vn>
2024-08-24 14:18:47 +0700

    lab2 VKU

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,91 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const IAmRich(),
+      home: const MiCartScreen(),
     );
   }
 }
 
-class IAmRich extends StatefulWidget {
-  const IAmRich({super.key});
+class MiCartScreen extends StatefulWidget {
+  const MiCartScreen({super.key});
 
   @override
-  State<IAmRich> createState() => _IAmRichState();
+  State<MiCartScreen> createState() => _MiCartScreenState();
 }
 
-class _IAmRichState extends State<IAmRich> {
+class _MiCartScreenState extends State<MiCartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 4, 14, 20),
-        title: Text(
-          "I Am Rich",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Container(
-        decoration: BoxDecoration(color: Colors.blueGrey),
-        child: Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
-          ),
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/me_sv.jpg'),
+            ),
+            Text(
+              "Nguyen Vinh",
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Flutter Developer",
+              style: TextStyle(
+                  fontFamily: 'Source Sans 3',
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100),
+            ),
+            SizedBox(
+              height: 20,
+              width: 150,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '0839519415',
+                  style: TextStyle(
+                      fontFamily: 'Source Sans 3',
+                      fontSize: 20,
+                      color: Colors.teal.shade900),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'nguyennvv.21it@vku.udn.vn',
+                  style: TextStyle(
+                      fontFamily: 'Source Sans 3',
+                      fontSize: 20,
+                      color: Colors.teal.shade900),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
